8647a22c5a85f878885d28f908bfbe5d397405af
ArmenAmirkhanian <ArmenAmirkhanian@users.noreply.github.com>
2015-05-19 23:02:13 -0500

    Added MIT license to code

--- PC_Generator/PC_Generator.cpp
+++ PC_Generator/PC_Generator.cpp
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Armen N. Amirkhanian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include <boost/spirit/include/qi.hpp>
 #include <fstream>
 #include <vector>
@@ -15,6 +39,8 @@ bool parse_into_vec(Iter p_it, Iter p_end, std::vector<int>& vf) {
 
 int main(int argc, char **args) {
 	std::string inputF;
+	
+	// Give the user the option to use the command line
 	if (argc < 2) {
 		std::cout << "Enter file name: ";
 		std::getline(std::cin, inputF);
